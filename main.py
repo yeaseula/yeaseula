cc2b6ee254a01dccf7902b3049c4e8cb6fb9fbbb
Yeaseul <103632412+yeaseula@users.noreply.github.com>
2025-07-17 18:10:57 +0900

    Update cont main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import feedparser, time
-
+ 
 URL="https://velog.io/@somv/posts/rss"
 RSS_FEED = feedparser.parse(URL)
-MAX_POST=5
-
+MAX_POST = 5
+ 
 markdown_text = """
 📝 My Latest Blog Posts
 """
-
-
+ 
 for idx, feed in enumerate(RSS_FEED['entries']):
     if idx > MAX_POST:
         break
     else:
         feed_date = feed['published_parsed']
         markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
-
-f = open("README.md",mode="w", encoding="utf-8")
+        
+f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
 f.close()
